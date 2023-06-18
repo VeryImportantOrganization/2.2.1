1216d01ab5ea37ac65e1c11568752847811226ca
phareebo <faribo.gafur@gmail.com>
2023-06-18 22:32:25 +0300

    first commit

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -11,19 +11,35 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        if (user.getCar() != null) {
+            sessionFactory.getCurrentSession().save(user.getCar());
+        }
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
 
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserWithCar(String model, int series) {
+        String HQL = "    SELECT users\n" +
+                "              FROM User users\n" +
+                "              JOIN users.car car\n" +
+                "              WHERE car.model = :model AND car.series = :series";
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(HQL, User.class);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+        return query.getSingleResult();
+    }
 }
